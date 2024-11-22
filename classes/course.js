70a1eb3e671be1636f432e84db4a07791f871223
Kelli Kirk <kelli.kirk@voco.ee>
2024-11-22 17:08:58 +0200

    Ylesanne 4 - parandused

--- classes/course.js
+++ classes/course.js
@@ -1,31 +1,26 @@
 class Course {
     constructor(courseName) {
         this.courseName = courseName;  // Kursuse nimi
-        this.grades = [];  // Õpilaste hinded
-    }
-
-    // Lisab õpilasele hinne kursusel
-    addStudentGrade(student, grade) {
-        this.grades.push({ student: student, grade: grade });
+        this.grades = new Map();  // Kasutame nüüd Map objekti
     }
 
     // Funktsioon, mis lisab õpilase hinde kursusele
     addGrade(student, grade) {
-        this.grades.set(student, grade);
+        this.grades.set(student, grade);  // Kasutame nüüd Map 'set' meetodit
     }
 
-    // Tagastab õpilaste hinded (massiiv objektidega)
+    // Tagastab õpilaste hinded (Map objekti järgi)
     getGrades() {
-        return this.grades;
+        return Array.from(this.grades, ([student, grade]) => ({ student, grade }));  // Muudame Map objekti massiiviks
     }
 
     // Arvutab kursuse keskmise hinde
-    getAverageGrade() { // Parandatud siin
-        if (this.grades.length === 0) {
+    getAverageGrade() {
+        if (this.grades.size === 0) {
             return -1;  // Kui hindeid pole, tagastame -1
         }
-        const total = this.grades.reduce((sum, gradeObj) => sum + gradeObj.grade, 0);
-        return total / this.grades.length;
+        const total = Array.from(this.grades.values()).reduce((sum, grade) => sum + grade, 0);
+        return total / this.grades.size;  // Kasutame Map size omadust
     }
 
     // Tagastab kursuse nime
@@ -73,9 +68,9 @@ student3.setId(103);
 const courseMath = new Course("Mathematics");
 
 // Lisame õpilastele hinded kursusel
-courseMath.addStudentGrade(student1, 4.5);  // Õpilane ja hinne
-courseMath.addStudentGrade(student2, 3.8);
-courseMath.addStudentGrade(student3, 4.2);
+courseMath.addGrade(student1, 4.5);  // Õpilane ja hinne
+courseMath.addGrade(student2, 3.8);
+courseMath.addGrade(student3, 4.2);
 
 // Kuvame kursuse info
 console.log(courseMath.description());  // Kursus: Mathematics
@@ -85,4 +80,5 @@ console.log(courseMath.getGrades());  // [{ student: Student { name: 'Jane Doe' 
 
 // Kuvame kursuse keskmise hinde
 console.log(courseMath.getAverageGrade());  // 4.166666666666667
+
 module.exports = Course;
